0e2410f1a59d6960c3962611104fe5cfa7d400dd
LiuCho <l.cabello@duocuc.cl>
2025-10-11 19:55:33 -0300

    Se sube la guia

--- Silkroad/src/Componentes/Navbar.jsx
+++ Silkroad/src/Componentes/Navbar.jsx
@@ -4,14 +4,14 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-custom-dark">
             <div className="container-fluid">
-                <img src="static/img/Logo Silk Road.png" alt="Logo Silk Road" className="navbar-logo" />
+                <img src="/img/Logo Silk Road.png" alt="Logo Silk Road" className="navbar-logo" />
 
                 <a className="navbar-brand" href="#">
-                    <img src="static/img/foro.png" alt="Foro" className="navbar-page-icon foro-icon" />
-                    <img src="static/img/guia.png" alt="Guia" className="navbar-page-icon guia-icon" />
-                    <img src="static/img/checklist.png" alt="Checklist" className="navbar-page-icon checklist-icon" />
-                    <img src="static/img/ranking.png" alt="Ranking" className="navbar-page-icon ranking-icon" />
-                    <img src="static/img/trivia.png" alt="Trivia" className="navbar-page-icon trivia-icon" />
+                    <img src="/img/foro.png" alt="Foro" className="navbar-page-icon foro-icon" />
+                    <img src="/img/guia.png" alt="Guia" className="navbar-page-icon guia-icon" />
+                    <img src="/img/checklist.png" alt="Checklist" className="navbar-page-icon checklist-icon" />
+                    <img src="/img/ranking.png" alt="Ranking" className="navbar-page-icon ranking-icon" />
+                    <img src="/img/trivia.png" alt="Trivia" className="navbar-page-icon trivia-icon" />
                 </a>
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -22,23 +22,23 @@ function Navbar() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/foro" ><img src="static/img/foro.png" alt="Foro" /></Link>
+                            <Link className="nav-link" to="/foro" ><img src="/img/foro.png" alt="Foro" /></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/guia" ><img src="static/img/guia.png" alt="Guia" /></Link>
+                            <Link className="nav-link" to="/guia" ><img src="/img/guia.png" alt="Guia" /></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/checklist"><img src="static/img/checklist.png"
+                            <Link className="nav-link" to="/checklist"><img src="/img/checklist.png"
                                     alt="Checklist" /></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/ranking"><img src="static/img/ranking.png" alt="Ranking" /></Link>
+                            <Link className="nav-link" to="/ranking"><img src="/img/ranking.png" alt="Ranking" /></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="trivia"><img src="static/img/trivia.png" alt="Trivia" /></Link>
+                            <Link className="nav-link" to="trivia"><img src="/img/trivia.png" alt="Trivia" /></Link>
                         </li>
                         <li className="nav-item cerrarSesion">
-                            <Link className="nav-link" to="/HomePage">Cerrar Sesión</Link>
+                            <Link className="nav-link" to="/"><img src="/img/cerrarsesion.png" alt="Cerrar Sesion"/></Link>
                         </li>
                     </ul>
                 </div>
