8067dd9191bac9966a5350da3187d9f501d1bb7c
LiuCho <l.cabello@duocuc.cl>
2025-10-23 23:45:46 -0300

    Cambios menores de diseño

--- Silkroad/src/main.jsx
+++ Silkroad/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style2.css'
 import './style.css'
